7accd63a4288ba9d2367137740324575c9c6cd9b
rodneytai <50794396+rodneytai@users.noreply.github.com>
2020-12-06 20:26:38 +0800

    Update GiftCard.vue
    decorate

--- src/components/GiftCard.vue
+++ src/components/GiftCard.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="container selfpos z-depth-5 lime lighten-5 border">
+    <div class="container selfpos z-depth-5 border">
         <div class="row inner-cutout">
             <div class="col s12 m12 l12 xl12 "> <!-- RWD class-->
-                <div class="">
-                    <div class="gifterSec knockout">
+                <div >
+                    <div class="gifterSec">
                         <!-- 送禮者 -->
                         <span class="">來自: {{ gifter }} 的祝福</span>
                     </div>
@@ -11,7 +11,7 @@
                         <!-- VIDEO from URL -->
                         <iframe style="width: 100%; height: 50vh;" v-bind:src="[videoURL]" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen='true' webkitallowfullscreen='true' mozallowfullscreen='true'></iframe>
                     </div>
-                    <div class="card-content greetingsSec z-depth-2">
+                    <div class="greetingsSec z-depth-3">
                         <!-- 賀詞/祝福語 -->
                         <p>{{ greetings }}</p>
                     </div>
@@ -35,6 +35,7 @@ export default{
 </script>
 
 <style scoped>
+/*For border animation to work*/
 @-webkit-keyframes Gradient {
     0% {
         background-position: 30% 50%
@@ -47,6 +48,7 @@ export default{
     }
     
 }
+/*Centering the whole div*/
 div .selfpos{
     /*position: absolute;*/
     /*top: 50%;*/
@@ -60,45 +62,26 @@ div .selfpos{
     justify-content: center;
     margin-top: 3%;    
 }
+/*Background Colour Animation*/
 .border{
     padding: 5px;
     background: linear-gradient(110deg, #ffeead 33%, rgba(0, 0, 0, 0) 33%), linear-gradient(110deg, #C5E7D7 34%, #88d8b0 34%);
     background-size: 400% 400%;
-    /*height: 400px;*/
     background-position: 25% 50%;
-    -webkit-animation: Gradient 15s ease infinite;
+    -webkit-animation: Gradient 10s ease infinite;
     -moz-animation: Gradient 5s ease infinite;
 }
+/*Inner BG Colour*/
 .inner-cutout{
-    /*padding: 40px 0;*/
     display: block;
     background-color:#ff6f69;    
     margin: 2%;
-    /*padding-bottom: 40px;*/
-    /*height: 300px;*/
-    background: radial-gradient(#ff6f69, #ff8b87);
-    background-size: 300%;
-    /*background-position:50% 50%;*/
+    padding-bottom: 20px;
+    background: radial-gradient(circle at top, #FF5F6D, #FFC371);
+    background-size: 100%;
+    background-position: 50% 50%;
 }
-.knockout {
-    
-    vertical-align: middle;
-    text-align: center;
-    font-family: 'Pacifico', cursive;
-    font-size:50pt;
-    color: blue;
-    color: #fff;
-    background: linear-gradient(110deg, #ffeead 33%, rgba(0, 0, 0, 0) 33%), linear-gradient(110deg, #C5E7D7 34%, #88d8b0 34%);
-    background-size: 400%;
-     -webkit-text-fill-color: transparent;
-  -webkit-background-clip: text;
-}
-.knockout{
-        animation: Gradient 5s ease infinite;
-        -webkit-animation: Gradient 15s ease infinite;
-        -moz-animation: Gradient 5s ease infinite;
-}
-
+/*Font setting for gifter and greetings*/
 .gifterSec, .greetingsSec{
     text-align: center;
     font-weight: bold;
@@ -108,15 +91,17 @@ div .selfpos{
     margin-top: 20px;
     margin-bottom: 20px;
     font-size: xx-large;
+    color: white;
+    text-shadow: 3px 3px 4px #db0000 ;
 }
 .greetingsSec{
     font-size: large;
+    color: white;    
+    background: linear-gradient(to left, #FF5F6D, #ff6f69);
+    background-size: 300%;
+    text-shadow: 1px 1px 3px red;
 }
 p{
     word-wrap: break-word;
-}
-.testtt{
-    background: rgb(34,193,195);
-    background: linear-gradient(180deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
 }
 </style>
